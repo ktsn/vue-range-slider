c3e608ecb50660b11ae908c249e5ebceb9c4b98e
Shinya Katayama <shinya.katayama@jp.oro.com>
2016-09-20 19:24:55 +0900

    Add example

---
+++ example/App.vue
@@ -0,0 +1,19 @@
+<template lang="html">
+  <div>
+    <range-slider min="50" max="300" step="50"></range-slider>
+  </div>
+</template>
+
+<script>
+import RangeSlider from 'vue-range-slider'
+import 'vue-range-slider/dist/vue-range-slider.css'
+
+export default {
+  data () {
+    return {}
+  },
+  components: {
+    RangeSlider
+  }
+}
+</script>
