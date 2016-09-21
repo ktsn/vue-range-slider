a279dca531a28636d43147cc2905ad71f6cf51f6
ktsn <ktsn55@gmail.com>
2016-09-21 09:49:22 +0900

    Add round util test

--- test/utils.js
+++ test/utils.js
@@ -2,8 +2,22 @@ import assert from 'power-assert'
 import { round } from '../src/utils'
 
 describe('utils', () => {
-  it('round', () => {
-    assert(round(-1, 0, 50, 1) === 0)
-    assert(round(11, 0, 10, 1) === 10)
+  describe('round', () => {
+    it('min, max', () => {
+      assert(round(-1, 0, 50, 2) === 0)
+      assert(round(11, 0, 10, 2) === 10)
+    })
+
+    it('do not touch the value on the multiple of step', () => {
+      assert(round(5, 0, 10, 5) === 5)
+    })
+
+    it('round to multiple of step', () => {
+      assert(round(10, 3, 12, 3) === 9)
+    })
+
+    it('fraction', () => {
+      assert(round(4.1, 0, 10, 1.5) === 4.5)
+    })
   })
 })
