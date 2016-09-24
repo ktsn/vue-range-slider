2f4014c899697c0ebe20d9ec2b2cc6aeca0ac858
ktsn <ktsn55@gmail.com>
2016-09-24 14:03:17 +0900

    Adjust example

--- example/App.vue
+++ example/App.vue
@@ -1,7 +1,43 @@
-<template lang="html">
-  <div>
-    <range-slider min="50" max="300" step="50"></range-slider>
-  </div>
+<template>
+  <form action="#" method="get">
+    <range-slider
+      v-model="value"
+      :name="name"
+      :min="min"
+      :max="max"
+      :step="step"
+      :disabled="disabled"></range-slider>
+
+    <div>
+      name
+      <input class="name" type="text" v-model="name">
+    </div>
+
+    <div>
+      value
+      <input class="value" type="number" v-model="value">
+    </div>
+
+    <div>
+      min
+      <input class="min" type="number" v-model="min">
+    </div>
+
+    <div>
+      max
+      <input class="max" type="number" v-model="max">
+    </div>
+
+    <div>
+      step
+      <input class="step" type="number" v-model="step">
+    </div>
+
+    <div>
+      disabled
+      <input class="disabled" type="checkbox" v-model="disabled">
+    </div>
+  </form>
 </template>
 
 <script>
@@ -10,7 +46,14 @@ import 'vue-range-slider/dist/vue-range-slider.css'
 
 export default {
   data () {
-    return {}
+    return {
+      name: null,
+      value: 0,
+      min: 0,
+      max: 100,
+      step: 1,
+      disabled: false
+    }
   },
   components: {
     RangeSlider
